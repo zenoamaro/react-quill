f17605eee1440e7563a8b575b54e818172770dcf
zenoamaro <zenoamaro@gmail.com>
2015-04-17 10:33:39 +0200

    Added a default color palette to the toolbar.

--- src/toolbar.js
+++ src/toolbar.js
@@ -3,6 +3,21 @@
 var React = require('react'),
 	T = React.PropTypes;
 
+var defaultColors = [
+	'rgb(  0,   0,   0)', 'rgb(230,   0,   0)', 'rgb(255, 153,   0)',
+	'rgb(255, 255,   0)', 'rgb(  0, 138,   0)', 'rgb(  0, 102, 204)',
+	'rgb(153,  51, 255)', 'rgb(255, 255, 255)', 'rgb(250, 204, 204)',
+	'rgb(255, 235, 204)', 'rgb(255, 255, 204)', 'rgb(204, 232, 204)',
+	'rgb(204, 224, 245)', 'rgb(235, 214, 255)', 'rgb(187, 187, 187)',
+	'rgb(240, 102, 102)', 'rgb(255, 194, 102)', 'rgb(255, 255, 102)',
+	'rgb(102, 185, 102)', 'rgb(102, 163, 224)', 'rgb(194, 133, 255)',
+	'rgb(136, 136, 136)', 'rgb(161,   0,   0)', 'rgb(178, 107,   0)',
+	'rgb(178, 178,   0)', 'rgb(  0,  97,   0)', 'rgb(  0,  71, 178)',
+	'rgb(107,  36, 178)', 'rgb( 68,  68,  68)', 'rgb( 92,   0,   0)',
+	'rgb(102,  61,   0)', 'rgb(102, 102,   0)', 'rgb(  0,  55,   0)',
+	'rgb(  0,  41, 102)', 'rgb( 61,  20,  10)',
+].map(function(color){ return { value: color } });
+
 var defaultItems = [
 
 	{ label:'Formats', type:'group', items: [
@@ -25,7 +40,8 @@ var defaultItems = [
 		{ type:'italic', label:'Italic' },
 		{ type:'strike', label:'Strike' },
 		{ type:'underline', label:'Underline' },
-		{ type:'link', label:'Link' }
+		{ type:'link', label:'Link' },
+		{ type:'color', label:'Color', items:defaultColors },
 	]},
 
 	{ label:'Blocks', type:'group', items: [
@@ -111,3 +127,5 @@ var QuillToolbar = React.createClass({
 });
 
 module.exports = QuillToolbar;
+QuillToolbar.defaultItems = defaultItems;
+QuillToolbar.defaultColors = defaultColors;
