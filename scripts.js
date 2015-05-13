5e7a56cce47dc34f1860432ea37c065651c01420
zenoamaro <zenoamaro@gmail.com>
2015-05-14 00:45:36 +0200

    Fixed small discrepancy between initial and rendered markup.

--- scripts.js
+++ scripts.js
@@ -5,33 +5,30 @@
 'use strict';
 
 
-var defaultContent = '\
-	<div><span style="font-size: 18px;">Quill Rich Text Editor</span></div>\
-	<div><br></div>\
-	<div>\
-		<span>Quill is a free, </span>\
-		<a href="https://github.com/quilljs/quill/">open source</a>\
-		<span> WYSIWYG editor built for the modern web. With its </span>\
-		<a href="http://quilljs.com/docs/modules/">extensible architecture</a>\
-		<span> and a </span>\
-		<a href="http://quilljs.com/docs/api/">expressive API</a>\
-		<span> you can completely customize it to fulfill your needs. Some built in features include:</span>\
-	</div>\
-	<div><br></div>\
-	<ul>\
-		<li>Fast and lightweight</li>\
-		<li>Semantic markup</li>\
-		<li>Standardized HTML between browsers</li>\
-		<li>Cross browser support including Chrome, Firefox, Safari, and IE 9+</li>\
-	</ul>\
-	<div><br></div>\
-	<div><span style="font-size: 18px;">Downloads</span></div>\
-	<div><br></div>\
-	<ul>\
-		<li><a href="https://quilljs.com">Quill.js</a>, the free, open source WYSIWYG editor</li>\
-		<li><a href="https://zenoamaro.github.io/react-quill">React-quill</a>, a React component that wraps Quill.js</li>\
-	</ul>\
-';
+var defaultContent = (
+	'<div><span style="font-size: 18px;">Quill Rich Text Editor</span></div>' +
+	'<div><br></div>' +
+	'<div>Quill is a free,' +
+	'    <a href="https://github.com/quilljs/quill/">open source</a>WYSIWYG editor built for the modern web. With its' +
+	'    <a href="http://quilljs.com/docs/modules/">extensible architecture</a>and a' +
+	'    <a href="http://quilljs.com/docs/api/">expressive API</a>you can completely customize it to fulfill your needs. Some built in features include:</div>' +
+	'<div><br></div>' +
+	'<ul>' +
+	'    <li>Fast and lightweight!</li>' +
+	'    <li>Semantic markup</li>' +
+	'    <li>Standardized HTML between browsers</li>' +
+	'    <li>Cross browser support including Chrome, Firefox, Safari, and IE 9+</li>' +
+	'</ul>' +
+	'<div><br></div>' +
+	'<div>' +
+	'    <span style="font-size: 18px;">Downloads</span>' +
+	'</div>' +
+	'<div><br></div>' +
+	'<ul>' +
+	'    <li><a href="https://quilljs.com">Quill.js</a>, the free, open source WYSIWYG editor</li>' +
+	'    <li><a href="https://zenoamaro.github.io/react-quill">React-quill</a>, a React component that wraps Quill.js</li>' +
+	'</ul>'
+);
 
 
 var defaultColors = ReactQuill.Toolbar.defaultColors;
