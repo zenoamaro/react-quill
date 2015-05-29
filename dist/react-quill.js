ae5ee920e6e9db9c4e71090a8e280e822141b086
zenoamaro <zenoamaro@gmail.com>
2015-05-29 19:54:04 +0200

    Forgot to bump version number in sources.

--- dist/react-quill.js
+++ dist/react-quill.js
@@ -58,7 +58,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /***/ function(module, exports, __webpack_require__) {
 
 	/*
-	React-Quill v0.1.0
+	React-Quill v0.1.1
 	https://github.com/zenoamaro/react-quill
 	*/
 	module.exports = __webpack_require__(/*! ./component */ 1);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /*
-React-Quill v0.1.0
+React-Quill v0.1.1
 https://github.com/zenoamaro/react-quill
 */
 module.exports = require('./component');
