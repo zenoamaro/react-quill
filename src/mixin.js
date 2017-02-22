8264fe8a7a8c694507a7d38b24f75e78a73a18aa
Alex Krolick <whokilledtheelectricmonk@gmail.com>
2017-02-21 21:50:42 -0800

    move handler declaration inside hookEditor to close over editor

--- src/mixin.js
+++ src/mixin.js
@@ -20,31 +20,30 @@ var QuillMixin = {
 		// accidentally modifying editor state.
 		var unprivilegedEditor = this.makeUnprivilegedEditor(editor);
 
+		this.handleEditorChange = function(delta, oldDelta, source) {
+			if (this.onEditorChange) {
+				this.onEditorChange(
+					editor.root.innerHTML, delta, source,
+					unprivilegedEditor
+				);
+				this.onEditorChangeSelection(
+					editor.getSelection(), source,
+					unprivilegedEditor
+				)
+			}
+		}.bind(this);
+
+		this.handleSelectionChange = function(range, oldRange, source) {
+			if (this.onEditorChangeSelection) {
+				this.onEditorChangeSelection(
+					range, source,
+					unprivilegedEditor
+				);
+			}
+		}.bind(this);
+
 		editor.on('editor-change', this.handleEditorChange);
-
 		editor.on('selection-change', this.handleSelectionChange);
-	},
-
-	handleEditorChange: function(delta, oldDelta, source) {
-		if (this.onEditorChange) {
-			this.onEditorChange(
-				editor.root.innerHTML, delta, source,
-				unprivilegedEditor
-			);
-			this.onEditorChangeSelection(
-				editor.getSelection(), source,
-				unprivilegedEditor
-			)
-		}
-	},
-
-	handleSelectionChange: function(range, oldRange, source) {
-		if (this.onEditorChangeSelection) {
-			this.onEditorChangeSelection(
-				range, source,
-				unprivilegedEditor
-			);
-		}
 	},
 
 	unhookEditor: function(editor) {
