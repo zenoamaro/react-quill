f6a7af963931b40e71a67f6de32a6a4bc6519f42
Alex Krolick <whokilledtheelectricmonk@gmail.com>
2017-02-25 19:08:25 -0800

    alert in demo if module is not installed and built

--- demo/index.js
+++ demo/index.js
@@ -2,6 +2,9 @@
 /* global ReactQuill */
 'use strict';
 
+if (typeof React !== 'object') alert('React not found. Did you run "npm install"?');
+if (typeof ReactQuill !== 'function') alert('ReactQuill not found. Did you run "make build"?')
+
 var Editor = React.createClass({
 
 	getInitialState: function() {
