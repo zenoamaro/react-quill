533711d569b3728e8c447856cfd8d850ea3e57b3
Alex Krolick <whokilledtheelectricmonk@gmail.com>
2017-02-26 00:03:55 -0800

    add more shimmed selection methods

--- test/polyfills/getSelection.js
+++ test/polyfills/getSelection.js
@@ -1,11 +1,20 @@
 module.exports = function(global) {
   global.document = global.document || {};
   global.window = global.window || {};
-  global.document.getSelection = global.window.getSelection || function() { 
-      return { 
-          getRangeAt: function() {},
-          removeAllRanges: function() {},
-      };
-  };
+  document.getSelection = getSelectionShim
+  document.createRange = document.getSelection;
 }
 
+/**
+ * DOM Traversal is not implemented in JSDOM
+ * The best we can do is shim the functions
+ */
+function getSelectionShim() {
+  return {
+    getRangeAt: function() {},
+    removeAllRanges: function() {},
+    setStart: function() {},
+    setEnd: function() {},
+    addRange: function() {},
+  };
+};
