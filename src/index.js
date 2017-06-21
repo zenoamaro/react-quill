2a4a275c729883740da34918888d7fc464d7970a
Alex Krolick <alexander.krolick@gmail.com>
2017-06-21 12:14:28 -0700

    remove custom inline size and font formats
    - Quill now has class-based Parchment formats
    - fixes #216

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ React-Quill v1.0.0
 https://github.com/zenoamaro/react-quill
 */
 var Quill = require('quill');
-var Parchment = Quill.import('parchment');
-var QuillStyle = Parchment.Attributor.Style;
-var styleOptions = { scope: Parchment.Scope.INLINE };
-Quill.register(new QuillStyle('size', 'font-size', styleOptions), true);
-Quill.register(new QuillStyle('font', 'font-family', styleOptions), true);
 
 module.exports = require('./component');
 module.exports.Quill = Quill;
