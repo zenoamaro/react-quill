f6baa0727c106243b3babaa235b7760d2b74b1e5
Drew Showalter <maelstroswim@gmail.com>
2018-06-27 15:20:28 +1000

    :recycle: add back ts-compatible default export

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ https://github.com/zenoamaro/react-quill
 var Quill = require('quill');
 
 module.exports = require('./component');
+module.exports.default = require('./component');
 module.exports.Quill = Quill;
 module.exports.Mixin = require('./mixin');
 module.exports.Toolbar = require('./toolbar');
