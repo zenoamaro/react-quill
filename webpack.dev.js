b54bdab2dc8cd2b690c99badf1d642dd38545af8
Alex Krolick <alexander.krolick@gmail.com>
2018-06-28 17:10:01 -0700

    Add babel-loader to webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -19,7 +19,16 @@ module.exports = {
 	module: {
 		// Shut off warnings about using pre-built javascript files
 		// as Quill.js unfortunately ships one as its `main`.
-		noParse: /node_modules\/quill\/dist/
+		noParse: /node_modules\/quill\/dist/,
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+				}
+			}
+		],
 	},
 
 	externals: {
