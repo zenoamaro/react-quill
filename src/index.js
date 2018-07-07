496ccd2611420dcac15dce28996102ad68a13432
Alex Krolick <akrolick@box.com>
2018-07-07 14:11:50 -0700

    Add default export (#374)

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ React-Quill v1.0.0
 https://github.com/zenoamaro/react-quill
 */
 var Quill = require('quill');
+var Component = require('./component');
 
-module.exports = require('./component');
+module.exports = Component;
+module.exports.default = Component;
 module.exports.Quill = Quill;
 module.exports.Mixin = require('./mixin');
 module.exports.Toolbar = require('./toolbar');
