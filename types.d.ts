85d6352141c523d1364f91d87e54169b008c3987
Max Gale <mgale4@binghamton.edu>
2019-06-15 23:57:11 -0400

    Update types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as Quill from "quill";
 
-declare namespace ReactQuill {
+export namespace ReactQuill {
 	export interface UnprivilegedEditor {
 		getLength(): number;
 		getText(index?: number, length?: number): string;
