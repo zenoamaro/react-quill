d47485a806a3fad43ef1643a42bf4c0b364cc0ae
Alex Krolick <alexander.krolick@gmail.com>
2020-04-09 02:16:34 -0700

    add more e2e tests
    - instead of using demo page, use a test harness that allows more inspection of the editor instance
    - run the old test suite inside the browser (!! it works, just requires one test to be rewritten to fully catch errors instead of hiding them)

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,43 +1,177 @@
 // load Cypress TypeScript definitions for IntelliSense
 /// <reference types="cypress" />
 
-beforeEach(() => {
-  cy.visit("/");
+import React from "react";
+import ReactDOM from "react-dom";
+import Quill from "quill";
+import ReactQuill from "../lib";
+
+Cypress.Commands.add("renderWithProps", (props = {}) => {
+  return cy.window().then(w => {
+    const target = w.document.getElementById("app");
+    function TestHarness() {
+      const [editorProps, setEditorProps] = React.useState(props);
+      w.setProps = setEditorProps;
+      w.editorRef = React.useRef();
+      return <ReactQuill {...editorProps} ref={w.editorRef} />;
+    }
+    ReactDOM.render(<TestHarness />, target);
+  });
 });
 
-it("can type in editor", function() {
-  cy.get(".ql-editor")
-    .type("hello world\n\nfoo bar")
-    .should("contain", "hello world")
-    .should("contain", "foo bar");
+Cypress.Commands.add("setProps", (props = {}) => {
+  return cy.window().then(window => {
+    // Hook into function provided by test harness:
+    window.setProps(props);
+  });
 });
 
-it("cannot type in read-only editor", function() {
-  cy.get("button")
-    .contains("Set read-only")
-    .click();
-
-  cy.get(".ql-editor").should("have.attr", "contenteditable", "false");
+// Returns the editor contenteditable node
+Cypress.Commands.add("getEditor", () => {
+  return cy.get(".ql-editor");
 });
 
-it("does not render container if disabled", function() {
-  cy.get("button")
-    .contains("Disable")
-    .click();
+// Returns a reference to the editor react element
+Cypress.Commands.add("getEditorRef", () =>
+  cy.window().then(w => w.editorRef.current)
+);
 
-  cy.get(".ql-editor").should("not.exist");
+describe("<ReactQuill />", () => {
+  beforeEach(() => {
+    cy.visit("/test.html");
+  });
+
+  it("can type in editor", function() {
+    cy.renderWithProps({});
+    cy.getEditor()
+      .type("hello world\n\nfoo bar")
+      .should("contain", "hello world")
+      .should("contain", "foo bar");
+  });
+
+  it("can set value with HTML string", function() {
+    const text = "Hello, world";
+    cy.renderWithProps({
+      value: `<p>${text}</p>`
+    });
+    cy.getEditor().should("contain", text);
+  });
+
+  it("can set value with Quill delta", function() {
+    const text = "Hello, world";
+    const delta = { ops: [{ insert: text }] };
+    cy.renderWithProps({
+      value: delta
+    });
+    cy.getEditor().should("contain", text);
+  });
+
+  it("allows using Deltas as defaultValue", () => {
+    const text = "Hello, world";
+    const delta = { ops: [{ insert: text }] };
+    cy.renderWithProps({
+      defaultValue: delta
+    });
+    cy.getEditor().should("contain", text);
+  });
+
+  it("cannot type in read-only editor", function() {
+    cy.renderWithProps({ readOnly: true });
+    cy.getEditor().should("have.attr", "contenteditable", "false");
+  });
+
+  it("preserves content when switching to read-only mode", function() {
+    cy.renderWithProps({});
+    cy.getEditor().as("editor");
+    cy.get("@editor")
+      .type("some text")
+      .should("contain", "some text");
+    cy.setProps({ readOnly: true });
+    cy.get("@editor").should("contain", "some text");
+  });
+
+  it("calls onChange after typing", () => {
+    const props = {
+      onChange: (_, delta) => {},
+      value: "<p>Hello, world</p>"
+    };
+    cy.spy(props, "onChange");
+    cy.renderWithProps(props);
+    cy.getEditor()
+      .type("!!!")
+      .then(() => {
+        expect(props.onChange).to.be.called;
+      });
+  });
+
+  it("can imperatively focus and blur using ref .blur() and .focus()", () => {
+    cy.renderWithProps();
+    cy.getEditor()
+      .type("hello")
+      .then(editor => {
+        cy.getEditorRef().then(reactquill => {
+          reactquill.blur();
+          expect(editor).to.not.be.focused;
+          reactquill.focus();
+          expect(editor).to.be.focused;
+        });
+      });
+  });
+
+  it("can access underlying Quill instance using ref .getEditor()", () => {
+    cy.renderWithProps();
+    cy.getEditorRef().then(reactquill => {
+      expect(reactquill.getEditor()).to.be.instanceOf(Quill);
+    });
+  });
+
+  it("passes options to Quill from props", () => {
+    const enabledFormats = ["underline", "bold", "italic"];
+    const props = {
+      placeholder: "foobar",
+      readOnly: true,
+      formats: enabledFormats,
+      modules: {
+        toolbar: enabledFormats
+      }
+    };
+    cy.renderWithProps(props);
+    cy.getEditorRef().then(reactquill => {
+      const quill = reactquill.getEditor();
+      expect(quill.options.placeholder).to.equal(props.placeholder);
+      expect(quill.options.readOnly).to.equal(props.readOnly);
+      expect(quill.options.modules).to.include.keys(Object.keys(props.modules));
+      expect(quill.options.formats).to.include.members(props.formats);
+    });
+  });
+
+  it("prevents using Delta changesets from events as value", () => {
+    const value = "<p>Hello, world</p>";
+    const changedValue = "Adieu, world!";
+    let lastDelta;
+    const onChange = (_, delta) => {
+      lastDelta = delta;
+    };
+    cy.renderWithProps({ value, onChange });
+    cy.getEditor()
+      .type("?")
+      .window(w => {
+        expect(() => w.setProps({ value: lastDelta })).to.throw(
+          "You are passing the `delta` object from the `onChange` event back"
+        );
+      });
+  });
 });
 
-it("preserves content when switching to read-only mode", function() {
-  cy.get(".ql-editor").as("editor");
+describe("old test suite", () => {
+  // Instead of running the old test inside JSDOM, run
+  // the tests inside the browser environment.
+  // This works because we are using Chai and Mocha,
+  // just like Cypress.
 
-  cy.get("@editor")
-    .type("some text")
-    .should("contain", "some text");
+  const Enzyme = require("enzyme");
+  const EnzymeAdapter = require("enzyme-adapter-react-16");
+  Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-  cy.get("button")
-    .contains("Set read-only")
-    .click();
-
-  cy.get("@editor").should("contain", "some text");
+  require("./index.js");
 });
--- test/setup.js
+++ test/setup.js
@@ -6,16 +6,18 @@
  * https://github.com/airbnb/enzyme/blob/master/docs/api/mount.md
  */
 
-require('jsdom-global')()
-require('./polyfills/MutationObserver.js')(global)
-require('./polyfills/getSelection.js')(global)
+require('jsdom-global')();
+require('./polyfills/MutationObserver.js')(global);
+require('./polyfills/getSelection.js')(global);
 
 // Configure the Enzyme adapter
 const Enzyme = require('enzyme');
 const EnzymeAdapter = require('enzyme-adapter-react-16');
-Enzyme.configure({adapter: new EnzymeAdapter()});
+Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 // Setup Chai to use Enzyme
 const chai = require('chai');
 const chaiEnzyme = require('chai-enzyme');
 chai.use(chaiEnzyme());
+
+global.expect = chai.expect;
