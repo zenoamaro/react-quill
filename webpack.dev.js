3c2ead4daa095c035afdb6d8215922d9914ff4de
Jordan Greissman <jordan@explo.co>
2023-02-09 15:11:08 -0500

    stuff

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,51 +1,48 @@
-'use strict';
-var webpack = require('webpack');
+"use strict";
+var webpack = require("webpack");
 
 module.exports = {
+  entry: "./src/index.js",
 
-	entry: './src/index.js',
+  devtool: "source-map",
 
-	debug: true,
-	devtool: 'module-source-map',
+  output: {
+    pathinfo: true,
+    filename: "./dist/react-quill.js",
+    library: "ReactQuill",
+    libraryTarget: "umd",
+  },
 
-	output: {
-		pathinfo: true,
-		filename: './dist/react-quill.js',
-		library: 'ReactQuill',
-		libraryTarget: 'umd'
-	},
+  module: {
+    // Shut off warnings about using pre-built javascript files
+    // as Quill.js unfortunately ships one as its `main`.
+    noParse: /node_modules\/quill\/dist/,
+  },
 
-	module: {
-		// Shut off warnings about using pre-built javascript files
-		// as Quill.js unfortunately ships one as its `main`.
-		noParse: /node_modules\/quill\/dist/
-	},
-
-	externals: {
-		'react': {
-			'commonjs': 'react',
-			'commonjs2': 'react',
-			'amd': 'react',
-			'root': 'React'
-		},
-		'react-dom': {
-			'commonjs': 'react-dom',
-			'commonjs2': 'react-dom',
-			'amd': 'react-dom',
-			'root': 'ReactDOM'
-		},
-		'react-dom/server': {
-			'commonjs': 'react-dom/server',
-			'commonjs2': 'react-dom/server',
-			'amd': 'react-dom/server',
-			'root': 'ReactDOMServer'
-		},
-		'prop-types': {
-			'commonjs': 'prop-types',
-			'commonjs2': 'prop-types',
-			'amd': 'prop-types',
-			'root': 'PropTypes'
-		}
-	}
-
+  externals: {
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "react",
+      root: "React",
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "react-dom",
+      root: "ReactDOM",
+    },
+    "react-dom/server": {
+      commonjs: "react-dom/server",
+      commonjs2: "react-dom/server",
+      amd: "react-dom/server",
+      root: "ReactDOMServer",
+    },
+    "prop-types": {
+      commonjs: "prop-types",
+      commonjs2: "prop-types",
+      amd: "prop-types",
+      root: "PropTypes",
+    },
+  },
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,55 +1,56 @@
-'use strict';
-var webpack = require('webpack');
+"use strict";
+var webpack = require("webpack");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
+  entry: "./src/index.js",
 
-	entry: './src/index.js',
+  devtool: "source-map",
 
-	debug: false,
-	devtool: 'module-source-map',
+  output: {
+    filename: "./dist/react-quill.min.js",
+    library: "ReactQuill",
+    libraryTarget: "umd",
+  },
 
-	output: {
-		filename: './dist/react-quill.min.js',
-		library: 'ReactQuill',
-		libraryTarget: 'umd'
-	},
+  module: {
+    // Shut off warnings about using pre-built javascript files
+    // as Quill.js unfortunately ships one as its `main`.
+    noParse: /node_modules\/quill\/dist/,
+  },
 
-	module: {
-		// Shut off warnings about using pre-built javascript files
-		// as Quill.js unfortunately ships one as its `main`.
-		noParse: /node_modules\/quill\/dist/
-	},
+  optimization: {
+    chunkIds: "total-size",
+    moduleIds: "size",
+    minimizer: [new TerserPlugin()],
+  },
 
-	externals: {
-		'react': {
-			'commonjs': 'react',
-			'commonjs2': 'react',
-			'amd': 'react',
-			'root': 'React'
-		},
-		'react-dom': {
-			'commonjs': 'react-dom',
-			'commonjs2': 'react-dom',
-			'amd': 'react-dom',
-			'root': 'ReactDOM'
-		},
-		'react-dom/server': {
-			'commonjs': 'react-dom/server',
-			'commonjs2': 'react-dom/server',
-			'amd': 'react-dom/server',
-			'root': 'ReactDOMServer'
-		},
-		'prop-types': {
-			'commonjs': 'prop-types',
-			'commonjs2': 'prop-types',
-			'amd': 'prop-types',
-			'root': 'PropTypes'
-		}
-	},
+  externals: {
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "react",
+      root: "React",
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "react-dom",
+      root: "ReactDOM",
+    },
+    "react-dom/server": {
+      commonjs: "react-dom/server",
+      commonjs2: "react-dom/server",
+      amd: "react-dom/server",
+      root: "ReactDOMServer",
+    },
+    "prop-types": {
+      commonjs: "prop-types",
+      commonjs2: "prop-types",
+      amd: "prop-types",
+      root: "PropTypes",
+    },
+  },
 
-	plugins: [
-		new webpack.optimize.UglifyJsPlugin(),
-		new webpack.optimize.OccurenceOrderPlugin()
-	]
-
+  plugins: [],
 };
